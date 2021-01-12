b3012791939d38c4bb52c07d9b4b31df1af12d8f
JamesMODonnell <james.odonnell401@gmail.com>
2021-01-12 13:00:35 -0500

    initial commit, making progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+const launchButton = document.querySelector("#launchButton");
+const confirmButton = document.querySelector("#confirmButton");
+const cancelButton = document.querySelector("#cancelButton");
+const launchButton = document.querySelector("#launchButton");
+const confirmButton = document.querySelector("#confirmButton");
+const cancelButton = document.querySelector("#cancelButton");
+const launchButton = document.querySelector("#launchButton");
+const confirmButton = document.querySelector("#confirmButton");
+const cancelButton = document.querySelector("#cancelButton");
+
+launchButton.addEventListener("click", function (event) {
+    console.log("the better way to add functionality");
+  });
+launchButton.addEventListener("click", function (event) {
+    console.log("the better way to add functionality");
+  });
+
+launchButton.addEventListener("click", function (event) {
+    console.log("the better way to add functionality");
+  });
+launchButton.addEventListener("click", function (event) {
+    console.log("the better way to add functionality");
+  });
